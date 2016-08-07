af8d2fa8e4bd4c0026a8bb0010ae40ddafe11fbc
roi <roi@saturn72.com>
2016-08-08 00:14:21 +0300

    Add Saturn72.Mappers code

---
+++ src/Saturn72.Mappers.Tests/SimpleMapperTests.cs
@@ -0,0 +1,122 @@
+﻿#region
+
+using NUnit.Framework;
+using Saturn72.UnitTesting.Framework;
+
+#endregion
+
+namespace Saturn72.Mappers.Tests
+{
+    public class SimpleMapperTests
+    {
+        [Test]
+        public void Map_SourceToDestination()
+        {
+            var source = new MapSource
+            {
+                ShouldNotMap_DiffPropName = "ShouldNotMap_DiffPropName",
+                ShouldNotMap_DiffPropType = "ShouldNotMap_DiffPropName",
+                ShouldNotMap_NotExists = "ShouldNotMap_NotExists",
+                String = "String",
+                Sub = new Source_SubType
+                {
+                    Int = int.MaxValue,
+                    Long = long.MaxValue
+                },
+                Should_Map_From_Int_To_TestEnum = 1,
+                Should_Map_From_TestEnum_To_Int = TestEnum.Value2
+            };
+
+            var dest = SimpleMapper.Map<MapSource, MapDestination>(source);
+
+            dest.String.ShouldEqual(source.String);
+            dest.ShouldNotMap_DiffPropName_.ShouldEqual(default(string));
+            dest.Sub.ShouldEqual(source.Sub);
+            dest.ShouldNotMap_DiffPropType.ShouldEqual(default(int));
+            dest.Should_Map_From_Int_To_TestEnum.ShouldEqual((TestEnum) source.Should_Map_From_Int_To_TestEnum);
+            dest.Should_Map_From_TestEnum_To_Int.ShouldEqual((int) source.Should_Map_From_TestEnum_To_Int);
+        }
+
+        [Test]
+        public void Map_SourceInstanceToDestinationInstance()
+        {
+            var source = new MapSource
+            {
+                ShouldNotMap_DiffPropName = "ShouldNotMap_DiffPropName",
+                ShouldNotMap_DiffPropType = "ShouldNotMap_DiffPropName",
+                ShouldNotMap_NotExists = "ShouldNotMap_NotExists",
+                String = "String",
+                Sub = new Source_SubType
+                {
+                    Int = int.MaxValue,
+                    Long = long.MaxValue
+                },
+                Should_Map_From_Int_To_TestEnum = 1,
+                Should_Map_From_TestEnum_To_Int = TestEnum.Value2
+            };
+
+            var dest = new MapDestination
+            {
+                String = "Thgis is init Value",
+                Should_Map_From_Int_To_TestEnum = TestEnum.Value2,
+                Should_Map_From_TestEnum_To_Int = int.MaxValue,
+            };
+
+            SimpleMapper.Map(source, dest);
+
+            dest.String.ShouldEqual(source.String);
+            dest.ShouldNotMap_DiffPropName_.ShouldEqual(default(string));
+            dest.Sub.ShouldEqual(source.Sub);
+            dest.ShouldNotMap_DiffPropType.ShouldEqual(default(int));
+            dest.Should_Map_From_Int_To_TestEnum.ShouldEqual((TestEnum) source.Should_Map_From_Int_To_TestEnum);
+            dest.Should_Map_From_TestEnum_To_Int.ShouldEqual((int) source.Should_Map_From_TestEnum_To_Int);
+        }
+
+
+
+        public enum TestEnum
+        {
+            Value1 = 1,
+            Value2 = 2
+        };
+
+        public class MapSource
+        {
+            public string String { get; set; }
+
+            public string ShouldNotMap_NotExists { get; set; }
+
+            public string ShouldNotMap_DiffPropName { get; set; }
+
+            public string ShouldNotMap_DiffPropType { get; set; }
+
+
+            public Source_SubType Sub { get; set; }
+            public int Should_Map_From_Int_To_TestEnum { get; set; }
+
+            public TestEnum Should_Map_From_TestEnum_To_Int { get; set; }
+        }
+
+        public class Source_SubType
+        {
+            public int Int { get; set; }
+
+            public long Long { get; set; }
+        }
+
+        public class MapDestination
+        {
+            public TestEnum Should_Map_From_Int_To_TestEnum { get; set; }
+
+            public int Should_Map_From_TestEnum_To_Int { get; set; }
+
+            public string String { get; set; }
+
+            public Source_SubType Sub { get; set; }
+
+            public string ShouldNotMap_DiffPropName_ { get; set; }
+
+            public int ShouldNotMap_DiffPropType { get; set; }
+        }
+    }
+}
---
+++ src/SimpleMapper.cs
@@ -0,0 +1,136 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+#endregion
+
+namespace Saturn72.Mappers
+{
+    public class SimpleMapper
+    {
+        public static TDestination Map<TSource, TDestination>(TSource source) where TSource : class
+            where TDestination : class
+        {
+            var propDictionary = ConvertInstanceToPropertyDictionary(source);
+
+            return Map<TDestination>(propDictionary);
+        }
+
+        public static TDestination Map<TDestination>(IDictionary<string, Func<object>> srcPropsDictionary)
+            where TDestination : class
+        {
+            var instance = Activator.CreateInstance<TDestination>();
+            return Map(srcPropsDictionary, instance);
+        }
+
+        public static TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+            where TSource : class
+            where TDestination : class
+        {
+            var propDictionary = ConvertInstanceToPropertyDictionary(source);
+
+            return Map(propDictionary, destination);
+        }
+
+
+        public static TDestination Map<TDestination>(IDictionary<string, Func<object>> srcPropsDictionary,
+            TDestination instance)
+        {
+            foreach (var key in srcPropsDictionary.Keys)
+            {
+                var pInfo = typeof(TDestination).GetProperty(key,
+                    BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                if (pInfo == null)
+                    continue;
+
+                var mapLogic = MapLogic.N_A;
+
+                var value = srcPropsDictionary[key]();
+
+                if (pInfo != null && value != null
+                    && pInfo.CanWrite && srcPropsDictionary.ContainsKey(key)
+                    && TypesCanBeMapped(value.GetType(), pInfo.PropertyType, ref mapLogic))
+                {
+                    switch (mapLogic)
+                    {
+                        case MapLogic.TypeToType:
+                            pInfo.SetValue(instance, value);
+                            continue;
+                        case MapLogic.IntToEnum:
+                            pInfo.SetValue(instance, Enum.ToObject(pInfo.PropertyType, value));
+                            continue;
+                            ;
+                        case MapLogic.EnumToInt:
+                            pInfo.SetValue(instance, (int) value);
+                            continue;
+                            ;
+                        case MapLogic.ObjectToString:
+                            pInfo.SetValue(instance, value.ToString());
+                            continue;
+                    }
+                }
+            }
+            return instance;
+        }
+
+        private static bool TypesCanBeMapped(Type sourceType, Type destType, ref MapLogic mapLogic)
+        {
+            if (sourceType == destType
+                || IsNullableToObjectOrObjectToNullable(sourceType, destType)
+                || IsNullableToObjectOrObjectToNullable(destType, sourceType)
+                || destType.IsAssignableFrom(sourceType))
+            {
+                mapLogic = MapLogic.TypeToType;
+                return true;
+            }
+
+            if (sourceType == typeof(int) && destType.IsEnum)
+            {
+                mapLogic = MapLogic.IntToEnum;
+                return true;
+            }
+            if (sourceType.IsEnum && destType == typeof(int))
+            {
+                mapLogic = MapLogic.EnumToInt;
+                return true;
+            }
+
+            if (destType == typeof(string))
+            {
+                mapLogic = MapLogic.ObjectToString;
+                return true;
+            }
+
+            return false;
+        }
+
+        private static IDictionary<string, Func<object>> ConvertInstanceToPropertyDictionary<TSource>(TSource source)
+            where TSource : class
+        {
+            var allProps = source.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(p => p.CanRead);
+
+            return allProps.ToDictionary(prop=> prop.Name, prop => new Func<object>(()=> prop.GetValue(source)));
+        }
+
+        private static bool IsNullableToObjectOrObjectToNullable(Type sourceType, Type destType)
+        {
+            return destType.IsGenericType && destType.GetGenericTypeDefinition() == typeof(Nullable<>) &&
+                   Nullable.GetUnderlyingType(destType) == sourceType;
+        }
+
+        private enum MapLogic
+        {
+            N_A,
+            TypeToType,
+            IntToEnum,
+            EnumToInt,
+            ObjectToString
+        }
+    }
+}
