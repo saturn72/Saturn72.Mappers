a7f7ddf94c38310d240a1b01bb4f81e4ffc7f563
roi@saturn72.com <roi@saturn72.com>
2016-08-08 21:18:42 +0300

    nunit issue

--- build/myget.bat
+++ build/myget.bat
@@ -34,6 +34,9 @@ echo run unit tests from %testBin%
 
 set nunitDir=%pkgDir%\NUnit.ConsoleRunner.3.4.1\tools
 set nunitExe=nunit3-console.exe
+
+if exist %nunitDir%\%nunitExe%(
+echo file exist)else (echo file not exists)
 echo cd %nunitDir%
 cd %nunitDir%
 
