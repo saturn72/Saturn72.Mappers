e60b1811918b6eb339e45cceed3126d53f3a5aa5
roi@saturn72.com <roi@saturn72.com>
2016-08-08 20:58:06 +0300

    call nunit from batch

--- build/myget.bat
+++ build/myget.bat
@@ -29,10 +29,12 @@ if not "%errorlevel%"=="0" goto failure
 REM Unit tests
 echo install nunit runners to %pkgDir%
 call %nuget% install NUnit.Runners -OutputDirectory %pkgDir%
+
+echo run unit tests from %testBin%
 set nunitDir=%pkgDir%\NUnit.ConsoleRunner.3.4.1\tools
 set nunitExe=nunit3-console.exe
-cd %nunitDir%
-%nunitExe% /config:%config% %testBin%
+call cd %nunitDir%
+call %nunitExe% /config:%config% %testBin%
 if not "%errorlevel%"=="0" goto failure
 
 
